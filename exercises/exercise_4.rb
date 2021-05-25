0dbbbb408ecc328cb52cb12003c371a1e5dfe967
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-05-24 19:49:52 -0700

    update to employee and store class / create employees for each store.

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,9 +6,9 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-surrey_store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
-whistler_store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
-yaletown_store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+@store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+@store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+@store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 # stores = Store.all
 # puts Store.all.count # => outputs => 5
 
